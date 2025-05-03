f0b9c5444c02e3da23bc456931d2b61f5b94ece9
Dasaav-dsv <simplydasaav@gmail.com>
2025-05-03 12:17:21 +0200

    Branch out memory allocation functions without CSTL_Type reliance

---
+++ lib/internal/alloc_dispatch.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#ifndef CSTL_ALLOC_DISPATCH_H
+#define CSTL_ALLOC_DISPATCH_H
+
+#include "../alloc.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct CSTL_SmallAllocFrame {
+    void* pointer;
+    char _buf[512];
+    uintptr_t cookie;
+} CSTL_SmallAllocFrame;
+
+static inline void* CSTL_allocate(size_t size, size_t alignment, CSTL_Alloc* alloc) {
+    if (alloc == NULL) {
+#ifdef _CRTALLOCATOR
+        return _aligned_malloc(size, alignment);
+#else
+        return aligned_alloc(alignment, size);
+#endif
+    }
+
+    assert(alloc->aligned_alloc != NULL);
+
+    return alloc->aligned_alloc(alloc->opaque, size, alignment);
+}
+
+static inline void CSTL_free(void* memory, size_t size, size_t alignment, CSTL_Alloc* alloc) {
+    if (alloc == NULL) {
+#ifdef _CRTALLOCATOR
+        _aligned_free(memory);
+#else
+        free(memory);
+#endif
+        return;
+    }
+
+    assert(alloc->aligned_free != NULL);
+
+    alloc->aligned_free(alloc->opaque, memory, size, alignment);
+}
+
+static inline void* CSTL_small_alloc(CSTL_SmallAllocFrame* frame, size_t size, size_t alignment, CSTL_Alloc* alloc, uintptr_t cookie) {
+    size_t align_val  = alignment - 1;
+
+    uintptr_t frame_base = (uintptr_t)&frame->_buf;
+    uintptr_t alloc_base = (frame_base + align_val) & ~align_val;
+
+#ifndef NDEBUG
+    frame->cookie = frame_base ^ (uintptr_t)cookie;
+#endif
+
+    if (frame_base + sizeof(frame->_buf) >= alloc_base + size
+        && frame_base >= alloc_base) {
+        frame->pointer = (void*)alloc_base;
+    } else {
+        frame->pointer = CSTL_allocate(size, alignment, alloc);
+    }
+
+    return frame->pointer;
+}
+
+static inline void CSTL_small_free(CSTL_SmallAllocFrame* frame, size_t size, size_t alignment, CSTL_Alloc* alloc, uintptr_t cookie) {
+    uintptr_t alloc_base  = (uintptr_t)frame->pointer;
+    uintptr_t frame_base = (uintptr_t)&frame->_buf;
+
+    assert(frame->cookie == (frame_base ^ cookie));
+
+    if (frame_base + sizeof(frame->_buf) < alloc_base + size
+        || frame_base < alloc_base) {
+        CSTL_free(frame->pointer, size, alignment, alloc);
+    }
+
+    frame->pointer = NULL;
+}
+
+#endif
