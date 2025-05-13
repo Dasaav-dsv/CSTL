3e84672af94f1476f531db6ba12195b8dfc1d7d9
Dasaav <SimplyDasaav@gmail.com>
2025-05-13 18:20:06 +0200

    Simplify CSTL_Type* interface

--- lib/internal/type_ext.h
+++ lib/internal/type_ext.h
@@ -4,44 +4,26 @@
 #define CSTL_TYPE_EXT_H
 
 #include "../type.h"
-#include "rcp_division.h"
 
-#define CSTL_TYPE_INVALID 0b00000001
-
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
-static inline size_t CSTL_type_alignment(CSTL_TypeCRef type) {
-    return (size_t)1 << type->align_lg;
+inline size_t CSTL_alignof_type(CSTL_Type type) {
+    intptr_t packed  = (intptr_t)type;
+    intptr_t npacked = -packed;
+
+    return (size_t)(packed & npacked);
 }
 
-static inline bool CSTL_is_address_aligned(uintptr_t address, CSTL_TypeCRef type) {
-    return (address & (CSTL_type_alignment(type) - 1)) == 0;
-}
+inline size_t CSTL_sizeof_type(CSTL_Type type) {
+    size_t alignment = CSTL_alignof_type(type);
+    intptr_t packed  = (intptr_t)type;
 
-static inline size_t CSTL_type_divide_by_rcp(size_t dividend, CSTL_TypeCRef type) {
-    return CSTL_divide_by_rcp(dividend, type->size_rcp, type->size_rcp_sh, type->size);
-}
-
-static inline ptrdiff_t CSTL_type_divide_by_rcp_signed(ptrdiff_t dividend, CSTL_TypeCRef type) {
-    return CSTL_divide_by_rcp_signed(dividend, type->size_rcp, type->size_rcp_sh, type->size);
-}
-
-static inline size_t CSTL_type_remainder_by_rcp(size_t dividend, CSTL_TypeCRef type) {
-    return CSTL_remainder_by_rcp(dividend, type->size_rcp, type->size_rcp_sh, type->size);
-}
-
-static inline void* CSTL_pointer_add(const void* pointer, ptrdiff_t n, CSTL_TypeCRef type) {
-    return (void*)((intptr_t)pointer + (intptr_t)type->size * n);
-}
-
-static inline void* CSTL_pointer_sub(const void* pointer, ptrdiff_t n, CSTL_TypeCRef type) {
-    return CSTL_pointer_add(pointer, -n, type);
-}
-
-static inline ptrdiff_t CSTL_pointer_distance(const void* lhs, const void* rhs, CSTL_TypeCRef type) {
-    return CSTL_type_divide_by_rcp_signed((intptr_t)rhs - (intptr_t)lhs, type);
+    if (packed < 0) {
+        return (size_t)packed ^ alignment;
+    } else {
+        return (size_t)packed;
+    }
 }
 
 #endif
--- lib/type.h
+++ lib/type.h
@@ -5,374 +5,195 @@
 
 #if defined(__cplusplus)
 #include <cstddef>
-#include <cstdint>
 extern "C" {
 #else
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdint.h>
 #endif
 
 /**
- * Basic type info used in place of a type template parameter.
+ * Opaque pseudohandle to the size and alignment of a C type.
  * 
- * Two `CSTL_Type` instances are compatible iff their size and alignment
- * are equal and the bound functions can operate interchangeably.
+ * Defined with `CSTL_define_type`, it is valid if not equal to `NULL`.
  * 
- * It is a logic error to use incompatible `CSTL_Type` instances one
- * after another when manipulating an object.
+ * The size of the type must be less than or equal to `INTPTR_MAX`.
  * 
  */
-typedef struct CSTL_Type {
-    /**
-     * Size of the type in bytes, including any padding bytes.
-     * 
-     * Must be a non-zero multiple of `1 << align_lg`.
-     * 
-     */
-    size_t size;
-
-    /**
-     * Fixed point reciprocal of the type's size, used for
-     * fast division and remainder operations.
-     * 
-     * Calculated automatically by `CSTL_define_*_type`. 
-     * 
-     */
-    size_t size_rcp;
-
-    /**
-     * Right shift of the fixed point reciprocal of the type's size,
-     * used for fast division and remainder operations.
-     * 
-     * Calculated automatically by `CSTL_define_*_type`. 
-     * 
-     */
-    uint8_t size_rcp_sh;
-
-    /**
-     * Natural alignment of the type in bytes (log2).
-     * 
-     * Calculated automatically by `CSTL_define_*_type`.
-     * 
-     */
-    uint8_t align_lg;
-
-    /**
-     * Determines whether `move_from` can be called on a `void*` pointer
-     * to an object of this type.
-     * 
-     * Set by `CSTL_define_*_type` or manually.
-     * 
-     */
-    uint8_t use_move_from;
-
-    /**
-     * Opaque bitfield with unstable ABI.
-     * 
-     */
-    uint8_t internal_flags;
-
-    /**
-     * Bound copy constructor function.
-     * 
-     * It is NOT permitted to mutate the `source`.
-     * 
-     * If null a `memmove(new_instance, src, size)` will be used in its stead.
-     * 
-     */
-    void (*copy_from)(void* new_instance, const void* src);
-
-    /**
-     * Bound move constructor function.
-     * 
-     * It is permitted to mutate the `source`, therefore it is suitable
-     * for a move constructor.
-     * 
-     * Remember that the moved-from object must stay in a valid state
-     * and be able to be destroyed after this call.
-     * 
-     * If null a `memmove(new_instance, src, size)` will be used in its stead.
-     * 
-     */
-    void (*move_from)(void* new_instance, void* src);
-
-    /**
-     * Bound destructor function.
-     * 
-     * If null a `memset(instance, 0xDE, size)` will be used in its stead.
-     * 
-     */
-    void (*destroy)(void* instance);
-} CSTL_Type;
+typedef struct CSTL_SType* CSTL_Type;
 
 /**
- * Reference to a mutable `CSTL_TypeRef`.
+ * Destroy range.
+ * 
+ * Required to destroy objects in the range `[first, last)`.
+ * 
+ * After this call, all objects in the range `[first, last)` are
+ * treated as uninitialized memory.
+ * 
+ */
+typedef void (*CSTL_Drop)(void* first, void* last);
+
+/**
+ * Move range.
+ * 
+ * Required to write `last - first` objects to uninitialized memory at
+ * `dest` so that each of them is equal to the corresponding object in
+ * the range `[first, last)` before the operation.
+ * 
+ * After this call, all objects in the range `[first, last)` are
+ * treated as valid objects in an unspecified state.
+ * 
+ */
+typedef void (*CSTL_Move)(void* first, void* last, void* dest);
+
+/**
+ * Copy range.
+ * 
+ * Required to write `(last - first) / size` objects to uninitialized
+ * memory at `dest` so that each of them is equal to the corresponding
+ * object in the range `[first, last)`.
+ * 
+ * Note that a `CSTL_Copy` function satisfies the requirements of
+ * `CSTL_Move`.
+ * 
+ */
+typedef void (*CSTL_Copy)(const void* first, const void* last, void* dest);
+
+/**
+ * Fill range.
+ * 
+ * Required to write `(last - first) / size` copies of `instance`
+ * to uninitialized memory at `[first, last)` so that each of them
+ * is equal to the corresponding `instance`.
+ * 
+ */
+typedef void (*CSTL_Fill)(void* first, void* last, const void* value);
+
+/**
+ * Function table for a type that can be destroyed by calling `drop`
+ * on a range of objects of that type.
+ * 
+ */
+typedef struct CSTL_DropType {
+    CSTL_Drop drop;
+} CSTL_DropType;
+
+/**
+ * Function table for a type that can be moved by calling `move`
+ * on a range of objects of that type to another range, or destroyed.
+ * 
+ */
+typedef struct CSTL_MoveType {
+    CSTL_DropType drop_type;
+    CSTL_Move move;
+} CSTL_MoveType;
+
+/**
+ * Function table for a type that can be copied by calling `copy`
+ * on a range of objects of that type to another range, or moved, or destroyed.
+ * 
+ * It is valid for `copy` to bind the same function as `move_type.move`.
+ * 
+ */
+typedef struct CSTL_CopyType {
+    CSTL_MoveType move_type;
+    CSTL_Copy copy;
+    CSTL_Fill fill;
+} CSTL_CopyType;
+
+/**
+ * Reference to a const `CSTL_DropType`.
  * 
  * Must not be null.
  * 
  */
-typedef CSTL_Type* CSTL_TypeRef;
+typedef const CSTL_DropType* CSTL_DropTypeCRef;
 
 /**
- * Reference to a const `CSTL_TypeRef`.
+ * Reference to a const `CSTL_MoveType`.
  * 
  * Must not be null.
  * 
  */
-typedef const CSTL_Type* CSTL_TypeCRef;
+typedef const CSTL_MoveType* CSTL_MoveTypeCRef;
 
 /**
- * Type info used in place of a template parameter for a type that can be compared.
- * 
- * The type must establish an equivalence relation:
- * `is_eq(a, b) == true` => `is_eq(b, a) == true`,
- * `is_eq(a, b) == true` && `is_eq(b, c) == true` => `is_eq(a, c) == true`,
- * 
- * Base `CSTL_Type` behavior and size and alignment requirements are unchanged.
- * 
- */
-typedef struct CSTL_EqType {
-    /**
-     * The underlying type info.
-     * 
-     */
-    CSTL_Type base;
-
-    /**
-     * Bound "equality" binary function.
-     * 
-     * Must establish an equivalence relation.
-     * 
-     * If null a `memcmp(lhs, rhs, size) == 0` will be used in its stead.
-     * 
-     */
-    bool (*is_eq)(const void* lhs, const void* rhs);
-} CSTL_EqType;
-
-/**
- * Reference to a mutable `CSTL_EqType`.
+ * Reference to a const `CSTL_CopyType`.
  * 
  * Must not be null.
  * 
  */
-typedef CSTL_EqType* CSTL_EqTypeRef;
+typedef const CSTL_CopyType* CSTL_CopyTypeCRef;
 
 /**
- * Reference to a const `CSTL_EqType`.
+ * Compare two objects for equality.
  * 
- * Must not be null.
+ * The function must establish an equivalence relation:
+ * `a == b` => `b == a`, `a == b` && `b == c` => `a == c`
  * 
  */
-typedef const CSTL_EqType* CSTL_EqTypeCRef;
+typedef bool (*CSTL_IsEq)(const void* lhs, const void* rhs);
 
 /**
- * Type info used in place of a template parameter for a type that can be compared.
+ * Compare two objects for less than.
  * 
- * The type must establish at least a strict weak ordering:
- * `is_lt(a, b) == true` => `is_lt(b, a) == false`
- * `is_lt(a, b) == true` && `is_lt(b, c) == true` => `is_lt(a, c) == true`
+ * The function must establish at least a strict weak ordering:
+ * `a < b` => `b < a == false`
+ * `a < b` && `b < c` => `a < c`
  * 
- * Base `CSTL_EqType` behavior and size and alignment requirements are unchanged.
+ */
+typedef bool (*CSTL_IsLt)(const void* lhs, const void* rhs);
+
+/**
+ * Function table for a type instances of which can be ordered
+ * with respect to others in a string weak ordering.
  * 
  */
 typedef struct CSTL_CompType {
-    /**
-     * The underlying type info.
-     * 
-     */
-    CSTL_EqType base;
-
-    /**
-     * Bound "less than" binary function.
-     * 
-     * Must establish a strict weak ordering relation.
-     * 
-     * If null a `memcmp(lhs, rhs, size) < 0` will be used in its stead.
-     * 
-     */
-    bool (*is_lt)(const void* lhs, const void* rhs);
+    CSTL_IsEq is_eq;
+    CSTL_IsLt is_lt;
 } CSTL_CompType;
 
 /**
- * Reference to a mutable `CSTL_CompType`.
+ * Obtain a hash for an instance of an object.
  * 
- * Must not be null.
- * 
- */
-typedef CSTL_CompType* CSTL_CompTypeRef;
-
-/**
- * Reference to a const `CSTL_CompType`.
- * 
- * Must not be null.
+ * The relation `a == b` => `hash(a) == hash(b)` must hold.
  * 
  */
-typedef const CSTL_CompType* CSTL_CompTypeCRef;
+typedef size_t (*CSTL_Hash)(const void* instance);
 
 /**
- * Type info used in place of a template parameter for a type that can be hashed.
- * 
- * The relation `is_eq(a, b)` => `hash(a) == hash(b)` must hold.
- * 
- * Base `CSTL_EqType` behavior and size and alignment requirements are unchanged.
+ * Function table for a type instances of which can be hashed
+ * and compared for equality.
  * 
  */
 typedef struct CSTL_HashType {
-    /**
-     * The underlying type info.
-     * 
-     */
-    CSTL_EqType base;
-    
-    /**
-     * Bound hash function.
-     * 
-     * If `is_eq(a, b)` then `hash(a) == hash(b)`.
-     * 
-     * If null an STL `std::hash` conforming implementation will be used in its stead.
-     * 
-     */
-    size_t (*hash)(const void* instance);
+    CSTL_IsEq is_eq;
+    CSTL_Hash hash;
 } CSTL_HashType;
 
 /**
- * Reference to a mutable `CSTL_HashType`.
+ * Obtain a pseudohandle to the size and alignment of a C type.
  * 
- * Must not be null.
+ * If the below requirements are broken, the function returns
+ * an invalid handle equal to `NULL`:
+ * 1. `size` must be a non-zero multiple of `alignment`.
+ * 2. `alignment` must be a power of 2.
+ * 3. `size` must be less than or equal to `INTPTR_MAX`.
  * 
  */
-typedef CSTL_HashType* CSTL_HashTypeRef;
+CSTL_Type CSTL_define_type(size_t size, size_t alignment);
 
 /**
- * Reference to a const `CSTL_HashType`.
- * 
- * Must not be null.
+ * Get the size of a C type back from a packed pseudohandle representation.
  * 
  */
-typedef const CSTL_HashType* CSTL_HashTypeCRef;
+size_t CSTL_sizeof_type(CSTL_Type type);
 
 /**
- * All possible return values of `CSTL_define_*_type`.
+ * Get the alignment of a C type back from a packed pseudohandle representation.
  * 
  */
-typedef enum CSTL_TypeErr {
-    /**
-     * Other error, zero initialized state of `CSTL_TypeErr`.
-     * 
-     */
-    CSTL_TypeErr_Other,
-
-    /**
-     * No error.
-     * 
-     */
-    CSTL_TypeErr_Ok,
-
-    /**
-     * Improper size of type.
-     * 
-     * The size of a type must be a non-zero multiple of its alignment.
-     * 
-     * Cstl also imposes a maximum size `SIZE_MAX / 2` for optimization reasons.
-     * 
-     * It follows from the alignment requirement that an alignment of `SIZE_MAX / 2 + 1`
-     * is impossible, and `size + alignment` cannot overflow.
-     * 
-     */
-    CSTL_TypeErr_BadSize,
-
-    /**
-     * Improper alignment of type.
-     * 
-     * Types must have a non-zero power of 2 alignment.
-     * 
-     */
-    CSTL_TypeErr_Misaligned,
-
-    /**
-     * Pointer passed as `new_instance` was null.
-     * 
-     */
-    CSTL_TypeErr_NullPointer,
-} CSTL_TypeErr;
-
-/**
- * Define a type, writing it to `new_instance` and validating parameters.
- * 
- * `copy_from`, `move_from`, `destroy` may be null, in which case their default
- * behavior is invoked. See descriptions of these fields in `CSTL_Type`.
- * 
- * If `use_copy_from == false` and `use_move_from == false` most container
- * relocation functions will fail.
- * 
- */
-CSTL_TypeErr CSTL_define_type(
-    CSTL_Type* new_instance,
-    size_t size,
-    size_t alignment,
-    void (*copy_from)(void* new_instance, const void* src),
-    void (*move_from)(void* new_instance, void* src),
-    void (*destroy)(void* instance)
-);
-
-/**
- * Define a type, writing it to `new_instance` and validating parameters.
- * 
- * `copy_from`, `move_from`, `destroy`, `is_eq` may be null, in which case their default
- * behavior is invoked. See descriptions of these fields in `CSTL_Type`.
- * 
- * If `use_copy_from == false` and `use_move_from == false` most container
- * relocation functions will fail.
- * 
- */
-CSTL_TypeErr CSTL_define_eq_type(
-    CSTL_EqType* new_instance,
-    size_t size,
-    size_t alignment,
-    void (*copy_from)(void* new_instance, const void* src),
-    void (*move_from)(void* new_instance, void* src),
-    void (*destroy)(void* instance),
-    bool (*is_eq)(const void* lhs, const void* rhs)
-);
-
-/**
- * Define a type, writing it to `new_instance` and validating parameters.
- * 
- * `copy_from`, `move_from`, `destroy`, `is_eq`, `is_lt` may be null,
- * in which case their default behavior is invoked. See descriptions
- * of these fields in `CSTL_Type`.
- * 
- */
-CSTL_TypeErr CSTL_define_comp_type(
-    CSTL_CompType* new_instance,
-    size_t size,
-    size_t alignment,
-    void (*copy_from)(void* new_instance, const void* src),
-    void (*move_from)(void* new_instance, void* src),
-    void (*destroy)(void* instance),
-    bool (*is_eq)(const void* lhs, const void* rhs),
-    bool (*is_lt)(const void* lhs, const void* rhs)
-);
-
-/**
- * Define a type, writing it to `new_instance` and validating parameters.
- * 
- * `copy_from`, `move_from`, `destroy`, `is_eq`, `hash` may be null,
- * in which case their default behavior is invoked. See descriptions
- * of these fields in `CSTL_Type`.
- * 
- */
-CSTL_TypeErr CSTL_define_hash_type(
-    CSTL_HashType* new_instance,
-    size_t size,
-    size_t alignment,
-    void (*copy_from)(void* new_instance, const void* src),
-    void (*move_from)(void* new_instance, void* src),
-    void (*destroy)(void* instance),
-    bool (*is_eq)(const void* lhs, const void* rhs),
-    size_t (*hash)(const void* instance)
-);
+size_t CSTL_alignof_type(CSTL_Type type);
 
 #if defined(__cplusplus)
 }
